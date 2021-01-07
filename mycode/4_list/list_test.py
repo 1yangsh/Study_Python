13e353ba2c893599c22ff043c582127b94da0d78
1yangsh <ysh410@gmail.com>
2021-01-07 22:25:14 +0900

    list ex

--- mycode/4_list/list_test.py
+++ mycode/4_list/list_test.py
@@ -13,14 +13,14 @@ print(num_list == num_list2, num_list is num_list2)
 
 print(num_list, num_list2)
 num_list.sort()
-print(num_list, num_list2) # 두 리스트 모두 sorting 됨 (같은 주소값에 있기 때문에)
+print(num_list, num_list2)  # 두 리스트 모두 sorting 됨 (같은 주소값에 있기 때문에)
 num_list2 = [1, 2, 3, 4, 5]
 num_list2.sort()
 print(num_list, num_list2)
 
 print(type(num_list), num_list)
 print(num_list[0], num_list[0:3], num_list[3:])
-for idx, num in enumerate(num_list) :
+for idx, num in enumerate(num_list):
     print(idx, num)
 
 str_list = ['Python', 'Java', 'Kotlin', 'C++', 'Go']
@@ -41,15 +41,15 @@ print(str_list * 2)  # 리스트 2회 출력
 print('Scalar' in str_list)  # True or False
 print('Java' in str_list)
 
-for idx, val in enumerate(str_list) :
+for idx, val in enumerate(str_list):
     print(idx, val)
 
 mix_list = [100, 3.14, True, '파이썬']
-for mix in mix_list :
+for mix in mix_list:
     print(type(mix), mix)
 
 
-my_list = list('Python') # str -> list
+my_list = list('Python')  # str -> list
 print(type(my_list), my_list)
 
 my_list2 = 'Hello, Python'.split(',')
@@ -69,18 +69,3 @@ print(my_list3)
 str1, str2 = ['cc', 'dd']
 print(str1)
 print(str2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
